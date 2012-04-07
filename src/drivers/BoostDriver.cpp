c6fe782aaf270fea15dd07e44d56d5a384f16a00
Paolo Ambrosio <paolo.ambrosio+github@gmail.com>
2012-04-07 16:56:03 +0200

    Fixes issue #41

--- src/drivers/BoostDriver.cpp
+++ src/drivers/BoostDriver.cpp
@@ -31,20 +31,22 @@ public:
     void reset();
 
     // Formatter
-    void    log_start( std::ostream&, counter_t test_cases_amount) {};
-    void    log_finish( std::ostream&) {};
-    void    log_build_info( std::ostream&) {};
+    void log_start( std::ostream&, counter_t test_cases_amount) {};
+    void log_finish( std::ostream&) {};
+    void log_build_info( std::ostream&) {};
 
-    void    test_unit_start( std::ostream&, test_unit const& tu) {};
-    void    test_unit_finish( std::ostream&, test_unit const& tu, unsigned long elapsed) {};
-    void    test_unit_skipped( std::ostream&, test_unit const& tu) {};
+    void test_unit_start( std::ostream&, test_unit const& tu) {};
+    void test_unit_finish( std::ostream&, test_unit const& tu, unsigned long elapsed) {};
+    void test_unit_skipped( std::ostream&, test_unit const& tu) {};
 
-    void    log_exception( std::ostream&, log_checkpoint_data const&, execution_exception const& ex) {};
+    void log_exception( std::ostream&, log_checkpoint_data const&, execution_exception const& ex) {};
 
-    void    log_entry_start( std::ostream&, log_entry_data const&, log_entry_types let) {};
-    void    log_entry_value( std::ostream&, const_string value);
-    void    log_entry_value( std::ostream&, lazy_ostream const& value) {};
-    void    log_entry_finish( std::ostream&) {};
+    void log_entry_start( std::ostream&, log_entry_data const&, log_entry_types let) {};
+    void log_entry_value( std::ostream&, const_string value);
+    void log_entry_value( std::ostream&, lazy_ostream const& value) {};
+    void log_entry_finish( std::ostream&) {};
+
+    void log_exception(std::ostream&, const boost::unit_test::log_checkpoint_data&, boost::unit_test::const_string) {};
 
 private:
     std::stringstream description;
