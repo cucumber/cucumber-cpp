2a6cd5308a439455b013d733984b8a3a6df3fd2e
Kamil Strzempowicz <konserw@gmail.com>
2016-01-10 12:41:39 +0100

    Added script run.sh to cleanup previous build and build cucumber with specific ruby version (using rvm) Thern run test, features and example. Travis will now create builds also for osx and 3 different ruby versions. Travis script part  moved to travis.sh Travis will now use ubuntu trusty (14.04) for linux build

---
+++ run.sh
@@ -0,0 +1,13 @@
+rvm install 2.2
+ruby -v
+gem install bundler
+rm Gemfile.lock
+rm build -rf
+cmake -E make_directory build
+cmake -E chdir build cmake -DCUKE_ENABLE_EXAMPLES=on -DCUKE_DISABLE_BOOST_TEST=on ..
+cmake --build build
+cmake --build build --target test
+cmake --build build --target features
+build/examples/Calc/GTestCalculatorSteps >/dev/null &
+cucumber examples/Calc
+killall GTestCalculatorSteps 
