034c93645cbcfc0c7d32c2f15f521a136e4206ef
Kamil Strzempowicz <konserw@gmail.com>
2016-01-14 17:28:59 +0100

    move travis script part to travis.sh

---
+++ travis.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e #break script on non-zero exitcode from any command
+gem install bundler
+cmake -E make_directory build
+cmake -E chdir build cmake -DCUKE_ENABLE_EXAMPLES=on ..
+cmake --build build
+cmake --build build --target test
+cmake --build build --target features
+
+GTEST=build/examples/Calc/GTestCalculatorSteps 
+BOOST=build/examples/Calc/BoostCalculatorSteps
+if [ -f $GTEST ]; then
+    GTEST >/dev/null &
+    cucumber examples/Calc
+fi
+if [ -f $BOOST ]; then 
+    BOOST >/dev/null &
+    cucumber examples/Calc
+fi
+
