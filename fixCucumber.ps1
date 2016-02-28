a6b84c40798d4feaf802808b438f9ad0c69596c2
Kamil Strzempowicz <konserw@gmail.com>
2016-02-28 21:41:20 +0100

    fixCucumber ansicon for cucumber 2.0

--- fixCucumber.ps1
+++ fixCucumber.ps1
@@ -1,7 +1,7 @@
 If ($env:ruby_version -match "200") {
-    $path = "C:\Ruby200\lib\ruby\gems\2.0.0\gems\cucumber-1.3.20\lib\cucumber\formatter\ansicolor.rb"
+    $path = "C:\Ruby200\lib\ruby\gems\2.0.0\gems\cucumber-2.0.0\lib\cucumber\formatter\ansicolor.rb"
 } Else {
-    $path = "C:\Ruby193\lib\ruby\gems\1.9.1\gems\cucumber-1.3.20\lib\cucumber\formatter\ansicolor.rb"
+    $path = "C:\Ruby193\lib\ruby\gems\1.9.1\gems\cucumber-2.0.0\lib\cucumber\formatter\ansicolor.rb"
 }
 
 (Get-Content $path) -replace '\ST\DERR.puts %', '#nope' | Set-Content $path 
