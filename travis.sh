276aa68ce17df2c44b6c016c16725a6b697b9fcc
Paolo Ambrosio <git@paolo.ambrosio.name>
2016-03-26 17:17:28 +0000

    Removed Bundler from CMake script (left as a manual step)

--- travis.sh
+++ travis.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 set -e #break script on non-zero exitcode from any command
 gem install bundler
+bundle install
 
 cmake -E make_directory build
-if [ -z ${GMOCK_PATH+x} ]; then 
+if [ -z ${GMOCK_PATH+x} ]; then
     cmake -E chdir build cmake -DCUKE_ENABLE_EXAMPLES=on -DGMOCK_VER=${GMOCK_VER} ..
 else
     cmake -E chdir build cmake -DCUKE_ENABLE_EXAMPLES=on -DGMOCK_SRC_DIR=${GMOCK_PATH} ..
@@ -12,14 +13,13 @@ cmake --build build
 cmake --build build --target test
 cmake --build build --target features
 
-GTEST=build/examples/Calc/GTestCalculatorSteps 
+GTEST=build/examples/Calc/GTestCalculatorSteps
 BOOST=build/examples/Calc/BoostCalculatorSteps
 if [ -f $GTEST ]; then
     $GTEST >/dev/null &
     cucumber examples/Calc
 fi
-if [ -f $BOOST ]; then 
+if [ -f $BOOST ]; then
     $BOOST >/dev/null &
     cucumber examples/Calc
 fi
-
