3abd374ffd669c4a1b2d7b591583554122591b98
ALLARD Antoine <Antoine.ALLARD@murex.com>
2016-10-25 13:54:00 +0200

    Fixing non valid preprocessor warning on msvc

--- include/cucumber-cpp/defs.hpp
+++ include/cucumber-cpp/defs.hpp
@@ -1,4 +1,8 @@
-#warning Use of defs.hpp is deprecated, please use either autodetect.hpp or generic.hpp
+#ifdef __GNUC__
+    #warning "Use of defs.hpp is deprecated, please use either autodetect.hpp or generic.hpp"
+#else
+    #pragma message( "Use of defs.hpp is deprecated, please use either autodetect.hpp or generic.hpp" )
+#endif
 #include "internal/defs.hpp"
 #ifndef STEP_INHERITANCE
     #include "internal/drivers/GenericDriver.hpp"
