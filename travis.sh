2dedbd4156f2ef6bda94f4a8b2f1732abdb07eb4
Giel van Schijndel <me@mortis.eu>
2016-12-30 15:59:12 +0100

    CI: output CTest error log on failures

--- travis.sh
+++ travis.sh
@@ -3,6 +3,9 @@ set -e #break script on non-zero exitcode from any command
 gem install bundler
 bundle install
 
+CTEST_OUTPUT_ON_FAILURE=ON
+export CTEST_OUTPUT_ON_FAILURE
+
 cmake -E make_directory build
 if [ -z ${GMOCK_PATH+x} ]; then
     cmake -E chdir build cmake -DCUKE_ENABLE_EXAMPLES=on -DGMOCK_VER=${GMOCK_VER} ..
