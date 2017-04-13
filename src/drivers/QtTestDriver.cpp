288a6ad64d8218e704e88a776628b7bcf952d686
konserw <konserw@gmail.com>
2017-04-13 11:35:09 +0200

    Second review

--- src/drivers/QtTestDriver.cpp
+++ src/drivers/QtTestDriver.cpp
@@ -9,21 +9,18 @@ namespace internal {
 
 const InvokeResult QtTestStep::invokeStepBody() {
     QTemporaryFile file;
-    QString fileName;
-    if(!file.open()) {
+    if (!file.open()) {
         return InvokeResult::failure("Unable to open temporary file needed for this test");
     }
-    file.close();
 
     QtTestObject testObject(this);
     int returnValue = QTest::qExec(&testObject, QStringList() << "test" << "-o" << file.fileName());
-    if(returnValue == 0)
+    if (returnValue == 0)
         return InvokeResult::success();
     else
     {
-        file.open();
         QTextStream ts(&file);
-        return InvokeResult::failure(ts.readAll().toLocal8Bit());
+        return InvokeResult::failure(ts.readAll().toUtf8().data());
     }
 }
 
