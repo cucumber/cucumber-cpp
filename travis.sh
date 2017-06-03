28132a1041c489f447925745608c5f5a9c0ccd7f
Giel van Schijndel <me@mortis.eu>
2017-06-03 23:01:05 +0200

    Travis CI: check formatting
    Add a Travis CI check that will fail if _changed_ code doesn't adhere to
    this configured style. It will also display the changes required to
    adhere to the style.
    
    This fixes #151

--- travis.sh
+++ travis.sh
@@ -30,6 +30,19 @@ killXvfb () {
     fi
 }
 
+if [ -n "${FORMAT:-}" ]; then
+    # Reformat all code changed since this branch forked from the default branch
+    git fetch origin HEAD
+    if [ "${TRAVIS_PULL_REQUEST:-false}" = "false" ]; then
+        BASE_HEAD="$(git rev-parse FETCH_HEAD)"
+    else
+        BASE_HEAD="$(git merge-base FETCH_HEAD HEAD)"
+    fi
+    git clang-format-3.8 --binary=/usr/bin/clang-format-3.8 --style=file --commit="${BASE_HEAD}"
+    # Assert that all changes adhere to the asked for style
+    exec git diff --exit-code
+fi
+
 CTEST_OUTPUT_ON_FAILURE=ON
 export CTEST_OUTPUT_ON_FAILURE
 
