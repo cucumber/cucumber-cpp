cbb249302196876d6b74b026658d77e008e2542b
Giel van Schijndel <me@mortis.eu>
2017-08-18 13:28:28 +0200

    Travis, OSX: discover Qt regardless of version
    The hardcoded path to the Qt directory for OSX doesn't work (anymore)
    because it refers to a non-existent version. Instead we now try to
    autodetect the path that Qt is installed to and tell CMake to look
    there.

--- travis.sh
+++ travis.sh
@@ -8,11 +8,18 @@ bundle install
 CTEST_OUTPUT_ON_FAILURE=ON
 export CTEST_OUTPUT_ON_FAILURE
 
+# Make Qt discoverable by CMake on OSX
+for QT_DIR in /usr/local/Cellar/qt*/*; do
+    if [ -d "${QT_DIR}" ]; then
+        CMAKE_PREFIX_PATH="${CMAKE_PREFIX_PATH:+${CMAKE_PREFIX_PATH};}${QT_DIR}"
+    fi
+done
+
 cmake -E make_directory build
 cmake -E chdir build cmake \
     -G Ninja \
     -DCUKE_ENABLE_EXAMPLES=on \
-    ${QT_DIR:+"-DCMAKE_PREFIX_PATH=${QT_DIR}"} \
+    ${CMAKE_PREFIX_PATH:+"-DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}"} \
     ${VALGRIND_TESTS:+"-DVALGRIND_TESTS=${VALGRIND_TESTS}"} \
     ${GMOCK_PATH:-"-DGMOCK_VER=${GMOCK_VER}"} \
     ${GMOCK_PATH:+"-DGMOCK_SRC_DIR=${GMOCK_PATH}"} \
