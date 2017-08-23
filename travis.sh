2c38fadf38aebff187bcf479f58f8344b75b7f31
Kamil Strzempowicz <konserw@gmail.com>
2017-08-23 16:59:08 +0200

    remove features and examples from test coverage report

--- travis.sh
+++ travis.sh
@@ -87,6 +87,8 @@ if [ -n "${COVERALLS_SERVICE_NAME}" ]; then
     lcov --remove coverage.info "/usr/*" --output-file coverage.info
     lcov --remove coverage.info "${TRAVIS_BUILD_DIR}/build/*" --output-file coverage.info
     lcov --remove coverage.info "${TRAVIS_BUILD_DIR}/tests/*" --output-file coverage.info
+    lcov --remove coverage.info "${TRAVIS_BUILD_DIR}/features/*" --output-file coverage.info
+    lcov --remove coverage.info "${TRAVIS_BUILD_DIR}/examples/*" --output-file coverage.info
     lcov --remove coverage.info "${TRAVIS_BUILD_DIR}/include/json_spirit/*" --output-file coverage.info
     lcov --list coverage.info
     coveralls-lcov coverage.info
