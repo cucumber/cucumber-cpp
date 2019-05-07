cucumber/cucumber-cpp

commit a7ff906ab379a055f4d9d06e1efdf00fc9434e72

Regex: use iterators for match results
* Use `smatch` to match std::string instead of char pointer
* Use iterators to walk the results instead of integer indices
* Store position as ptrdiff_t to always have the correct size

This prevents signed/unsigned and narrowing (64-bit to 32-bit)
conversion warnings on MSVC.

src/Regex.cpp
@@ -25,14 +25,18 @@ boost::shared_ptr<RegexMatch> Regex::find(const std::string &expression) const {
 }
 
 FindRegexMatch::FindRegexMatch(const boost::regex &regexImpl, const std::string &expression) {
-    boost::cmatch matchResults;
-    regexMatched = boost::regex_search(expression.c_str(), matchResults, regexImpl, boost::regex_constants::match_extra);
+    boost::smatch matchResults;
+    regexMatched = boost::regex_search(
+        expression, matchResults, regexImpl, boost::regex_constants::match_extra);
     if (regexMatched) {
-        for (boost::cmatch::size_type i = 1; i < matchResults.size(); ++i) {
-            RegexSubmatch s;
-            s.value = matchResults.str(i);
-            s.position = matchResults.position(i);
-            submatches.push_back(s);
+        boost::smatch::const_iterator i = matchResults.begin();
+        if (i != matchResults.end())
+            ++i;
+        for (; i != matchResults.end(); ++i) {
+            if (i->matched) {
+                RegexSubmatch s = {*i, i->first - expression.begin()};
+                submatches.push_back(s);
+            }
         }
     }
 }
@@ -42,17 +46,13 @@ boost::shared_ptr<RegexMatch> Regex::findAll(const std::string &expression) cons
 }
 
 FindAllRegexMatch::FindAllRegexMatch(const boost::regex &regexImpl, const std::string &expression) {
-    regexMatched = false;
     boost::sregex_token_iterator i(expression.begin(), expression.end(), regexImpl, 1, boost::regex_constants::match_continuous);
-    boost::sregex_token_iterator j;
-    while (i != j) {
-        regexMatched = true;
-        RegexSubmatch s;
-        s.value = *i;
-        s.position = -1;
+    const boost::sregex_token_iterator end;
+    for (; i != end; ++i) {
+        RegexSubmatch s = {*i, -1};
         submatches.push_back(s);
-        ++i;
     }
+    regexMatched = !submatches.empty();
 }
 
 }

include/cucumber-cpp/internal/CukeEngine.hpp
@@ -1,6 +1,7 @@
 #ifndef CUKE_CUKEENGINE_HPP_
 #define CUKE_CUKEENGINE_HPP_
 
+#include <cstddef>
 #include <string>
 #include <vector>
 
@@ -12,7 +13,7 @@ namespace internal {
 class StepMatchArg {
 public:
     std::string value;
-    int position;
+    std::ptrdiff_t position;
 };
 
 class StepMatch {

include/cucumber-cpp/internal/utils/Regex.hpp
@@ -1,6 +1,7 @@
 #ifndef CUKE_REGEX_HPP_
 #define CUKE_REGEX_HPP_
 
+#include <cstddef>
 #include <vector>
 
 #include <boost/shared_ptr.hpp>
@@ -11,7 +12,7 @@ namespace internal {
 
 struct RegexSubmatch {
     std::string value;
-    int position;
+    std::ptrdiff_t position;
 };
 
 

src/connectors/wire/WireProtocol.cpp
@@ -268,7 +268,7 @@ namespace {
                 BOOST_FOREACH(StepMatchArg ma, m.args) {
                     mObject jsonMa;
                     jsonMa["val"] = ma.value;
-                    jsonMa["pos"] = ma.position;
+                    jsonMa["pos"] = static_cast<boost::int64_t>(ma.position);
                     jsonArgs.push_back(jsonMa);
                 }
                 jsonM["args"] = jsonArgs;

tests/unit/StepManagerTest.cpp
@@ -19,7 +19,7 @@ protected:
     const static char *another_matcher;
     const static char *no_match;
     const static char *a_third_matcher;
-    const map<int, string> no_params;
+    const map<std::ptrdiff_t, string> no_params;
 
     int getUniqueMatchIdOrZeroFor(const string &stepMatch) {
         MatchResult::match_results_type resultSet = getResultSetFor(stepMatch);
@@ -30,7 +30,7 @@ protected:
         }
     }
 
-    int countMatches(const string &stepMatch) {
+    size_t countMatches(const string &stepMatch) {
         return getResultSetFor(stepMatch).size();
     }
 
@@ -42,7 +42,7 @@ protected:
         return (countMatches(stepMatch) > 0);
     }
 
-    bool extractedParamsAre(const string stepMatch, map<int, string> params) {
+    bool extractedParamsAre(const string stepMatch, map<std::ptrdiff_t, string> params) {
         MatchResult::match_results_type resultSet = getResultSetFor(stepMatch);
         if (resultSet.size() != 1) {
             return false; // more than one match

commit e7b8f641ebf939380d7e6c8ce217b8cfcbf7e808

Fix regression on optional regex submatches
This regression was introduced in commit a7ff906 (Regex: use iterators
for match results).

src/Regex.cpp
@@ -37,6 +37,8 @@ FindRegexMatch::FindRegexMatch(const boost::regex &regexImpl, const std::string 
             if (i->matched) {
                 RegexSubmatch s = {*i, i->first - expression.begin()};
                 submatches.push_back(s);
+            } else {
+                submatches.push_back(RegexSubmatch());
             }
         }
     }

tests/unit/RegexTest.cpp
@@ -46,6 +46,22 @@ TEST(RegexTest, matchesRegexWithSubmatches) {
     EXPECT_EQ("69", match->getSubmatches()[1].value);
 }
 
+TEST(RegexTest, matchesRegexWithOptionalSubmatches) {
+    Regex sum("^(\\d+)\\+(\\d+)(?:\\+(\\d+))?=(\\d+)$");
+
+    shared_ptr<RegexMatch> match(sum.find("1+2+3=6"));
+    EXPECT_TRUE(match->matches());
+    ASSERT_EQ(4, match->getSubmatches().size());
+
+    match = shared_ptr<RegexMatch>(sum.find("42+27=69"));
+    EXPECT_TRUE(match->matches());
+    ASSERT_EQ(4, match->getSubmatches().size());
+    EXPECT_EQ("42", match->getSubmatches()[0].value);
+    EXPECT_EQ("27", match->getSubmatches()[1].value);
+    EXPECT_EQ("", match->getSubmatches()[2].value);
+    EXPECT_EQ("69", match->getSubmatches()[3].value);
+}
+
 TEST(RegexTest, findAllDoesNotMatchIfNoTokens) {
     Regex sum("([^,]+)(?:,|$)");
     shared_ptr<RegexMatch> match(sum.findAll(""));