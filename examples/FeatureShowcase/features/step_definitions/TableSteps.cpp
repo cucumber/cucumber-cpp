934e544d417822cf0c35d6cec028c04fc7b5cc95
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2020-02-28 06:47:46 +0100

    Added mode tests

--- examples/FeatureShowcase/features/step_definitions/TableSteps.cpp
+++ examples/FeatureShowcase/features/step_definitions/TableSteps.cpp
@@ -8,58 +8,115 @@ using cucumber::ScenarioScope;
 
 class ActiveActors {
 public:
-    typedef std::string actor_name_type;
-    typedef unsigned short actor_year_type;
+  typedef std::string actor_name_type;
+  typedef unsigned short actor_year_type;
 private:
-    typedef std::map<actor_name_type, actor_year_type> actors_type;
+  typedef std::map<actor_name_type, actor_year_type> actors_type;
 
-    actors_type actors;
+  actors_type actors;
 public:
-    void addActor(const actor_name_type name, const actor_year_type year) {
-        actors[name] = year;
-    }
+  void addActor(const actor_name_type name, const actor_year_type year) {
+    actors[name] = year;
+  }
 
-    void retireActor(const actor_name_type name) {
-        actors.erase(actors.find(name));
-    }
+  void retireActor(const actor_name_type name) {
+    actors.erase(actors.find(name));
+  }
 
-    actor_name_type getOldestActor() {
-        actors_type::iterator it = actors.begin();
-        actor_name_type name = it->first;
-        actor_year_type year = it->second;
-        while(++it != actors.end()) {
-            actor_year_type currentYear = it->second;
-            if (year > currentYear) {
-                name = it->first;
-            }
-        }
-        return name;
+  void killActor(const actor_name_type name) {
+    actors.erase(actors.find(name));
+  }
+
+  actor_name_type getOldestActor() {
+    actors_type::iterator it = actors.begin();
+    actor_name_type name = it->first;
+    actor_year_type year = it->second;
+    actor_year_type currentYear = 0;
+    while(it != actors.end()) {
+      currentYear = it->second;
+      if (year > currentYear) {
+        name = it->first;
+        year = it->second;
+      }
+      it++;
     }
+    return name;
+  }
+
+  actor_name_type getYoungestActor() {
+    actors_type::iterator it = actors.begin();
+    actor_name_type name = it->first;
+    actor_year_type bestYearOfBirth = it->second;
+    actor_year_type currentYearOfBirth = 0;
+    while(it != actors.end()) {
+      currentYearOfBirth = it->second;
+      if (bestYearOfBirth < currentYearOfBirth) {
+        name = it->first;
+        bestYearOfBirth = currentYearOfBirth;
+      }
+      it++;
+    }
+    return name;
+  }
+
+  actor_name_type getSexiestActor() {
+    actors_type::iterator it = actors.begin();
+    while(it != actors.end()) {
+      if (it->first == "George Clooney") {
+        return it->first;
+      }
+      it++;
+    }
+    return "nobody";
+  }
 };
 
 GIVEN("^the following actors are still active") {
-    TABLE_PARAM(actorsParam);
-    ScenarioScope<ActiveActors> context;
+  TABLE_PARAM(actorsParam);
+  ScenarioScope<ActiveActors> context;
 
-    const table_hashes_type & actors = actorsParam.hashes();
-    for (table_hashes_type::const_iterator ait = actors.begin(); ait != actors.end(); ++ait) {
-        std::string name(ait->at("name"));
-        std::string yearString(ait->at("born"));
-        const ActiveActors::actor_year_type year = ::cucumber::internal::fromString<ActiveActors::actor_year_type>(yearString);
-        context->addActor(name, year);
-    }
+  const table_hashes_type & actors = actorsParam.hashes();
+  for (table_hashes_type::const_iterator ait = actors.begin(); ait != actors.end(); ++ait) {
+    std::string name(ait->at("name"));
+    std::string yearString(ait->at("born"));
+    const ActiveActors::actor_year_type year = ::cucumber::internal::fromString<ActiveActors::actor_year_type>(yearString);
+    context->addActor(name, year);
+  }
 }
 
 WHEN("^(.+) retires") {
-    REGEX_PARAM(std::string, retiringActor);
-    ScenarioScope<ActiveActors> context;
+  REGEX_PARAM(std::string, retiringActor);
+  ScenarioScope<ActiveActors> context;
 
-    context->retireActor(retiringActor);
+  context->retireActor(retiringActor);
+}
+
+WHEN("^(.+) dies") {
+  REGEX_PARAM(std::string, retiringActor);
+  ScenarioScope<ActiveActors> context;
+
+  context->killActor(retiringActor);
 }
 
 THEN("^the oldest actor should be (.+)$") {
-    REGEX_PARAM(std::string, oldestActor);
-    ScenarioScope<ActiveActors> context;
+  REGEX_PARAM(std::string, oldestActor);
+  ScenarioScope<ActiveActors> context;
 
-    ASSERT_EQ(oldestActor, context->getOldestActor());
+  ASSERT_EQ(oldestActor, context->getOldestActor());
+}
+
+
+THEN("^the youngest actor should be (.+)$") {
+  REGEX_PARAM(std::string, youngestActor);
+  ScenarioScope<ActiveActors> context;
+
+  ASSERT_EQ(youngestActor, context->getYoungestActor());
+}
+
+
+THEN("^the sexiest actor should be (.+)$") {
+  REGEX_PARAM(std::string, sexiestActor);
+  ScenarioScope<ActiveActors> context;
+
+  ASSERT_EQ(sexiestActor, context->getSexiestActor());
 }
--- examples/FeatureShowcase/features/table.feature
+++ examples/FeatureShowcase/features/table.feature
@@ -3,12 +3,34 @@ Feature: Table
   In order to explain how to use tagbles
   I have to make this silly example
 
-  Scenario: No tag
+  Background:
     Given the following actors are still active:
       | name           | born |
       | Al Pacino      | 1940 |
       | Robert De Niro | 1943 |
       | George Clooney | 1961 |
       | Morgan Freeman | 1937 |
-    When Morgan Freeman retires
+
+  Scenario: Clooney dies
+    Given George Clooney dies
+    Then the sexiest actor should be nobody
+    And the youngest actor should be Robert De Niro
+    And the oldest actor should be Morgan Freeman
+
+  Scenario: Clooney retires
+    Given George Clooney retires
+    Then the sexiest actor should be nobody
+    And the youngest actor should be Robert De Niro
+    And the oldest actor should be Morgan Freeman
+
+  Scenario: Morgan retires
+    Given Morgan Freeman retires
     Then the oldest actor should be Al Pacino
+    And the sexiest actor should be George Clooney
+    And the youngest actor should be George Clooney
+
+  Scenario: Morgan dies
+    Given Morgan Freeman dies
+    Then the oldest actor should be Al Pacino
+    And the sexiest actor should be George Clooney
+    And the youngest actor should be George Clooney
