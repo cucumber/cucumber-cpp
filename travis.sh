5387438007e42b30c996e8c212e769212105696d
Urs Fässler <urs@bitzgi.ch>
2023-10-17 18:09:28 +0200

    make (most of) travis script work with Ubuntu 22.04
    E2E tests are not yet working.

--- travis.sh
+++ travis.sh
@@ -3,61 +3,26 @@ set -e #break script on non-zero exitcode from any command
 set -x #display command being executed
 
 startXvfb () {
-# Xvfb sends SIGUSR1 to its parent when it finished startup, this causes the 'wait' below to stop waiting
-# Starting Xvfb hangs on OSX, that's why we do this on Linux only now
-    if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
-        trap : USR1
-        (trap '' USR1; Xvfb $DISPLAY -screen 0 640x480x8 -nolisten tcp > /dev/null 2>&1) &
-        XVFBPID=$!
-        wait || :
-        trap '' USR1
-        if ! kill -0 $XVFBPID 2> /dev/null; then
-            echo "Xvfb failed to start" >&2
-            exit 1
-        fi
-    else
-        sudo Xvfb $DISPLAY -screen 0 640x480x8 -nolisten tcp > /dev/null 2>&1 &
-        XVFBPID=$!
-        sleep 5 
-    fi
+    Xvfb $DISPLAY -screen 0 640x480x8 -nolisten tcp > /dev/null 2>&1 &
+    XVFBPID=$!
+    sleep 5
 }
 
 killXvfb () {
     if [ -n "${XVFBPID:-}" ]; then
         # Stop virtual X display server
-        sudo kill $XVFBPID
-        wait
+        kill $XVFBPID
     fi
 }
 
-if [ -n "${FORMAT:-}" ]; then
-    # Reformat all code changed since this branch forked from the default branch
-    git fetch origin HEAD
-    if [ "${TRAVIS_PULL_REQUEST:-false}" = "false" ]; then
-        BASE_HEAD="$(git rev-parse FETCH_HEAD)"
-    else
-        BASE_HEAD="$(git merge-base FETCH_HEAD HEAD)"
-    fi
-    git clang-format-3.8 --binary=/usr/bin/clang-format-3.8 --style=file --commit="${BASE_HEAD}"
-    # Assert that all changes adhere to the asked for style
-    exec git diff --exit-code -- . ":!3rdparty"
-fi
-
 CTEST_OUTPUT_ON_FAILURE=ON
 export CTEST_OUTPUT_ON_FAILURE
-
-# Make Qt discoverable by CMake on OSX
-for QT_DIR in /usr/local/Cellar/qt*/*; do
-    if [ -d "${QT_DIR}" ]; then
-        CMAKE_PREFIX_PATH="${CMAKE_PREFIX_PATH:+${CMAKE_PREFIX_PATH};}${QT_DIR}"
-    fi
-done
 
 cmake -E make_directory build
 cmake -E chdir build cmake \
     -G Ninja \
     -DCUKE_ENABLE_EXAMPLES=on \
-    -DBUILD_SHARED_LIBS="${BUILD_SHARED_LIBS:-ON}" \
+    -DBUILD_SHARED_LIBS="${BUILD_SHARED_LIBS:-OFF}" \
     -DCMAKE_INSTALL_PREFIX=${HOME}/.local \
     ${CMAKE_PREFIX_PATH:+"-DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}"} \
     ${COVERALLS_SERVICE_NAME:+"-DCMAKE_BUILD_TYPE=Debug"} \
@@ -66,9 +31,11 @@ cmake -E chdir build cmake \
     ${GMOCK_PATH:-"-DGMOCK_VER=${GMOCK_VER}"} \
     ${GMOCK_PATH:+"-DGMOCK_SRC_DIR=${GMOCK_PATH}"} \
     ..
-cmake --build build
-cmake --build build --target test
-cmake --build build --target features
+cmake --build build --parallel
+cmake --build build --parallel --target test
+
+#TODO fix feature tests
+#cmake --build build --target features
 
 #
 # Execute Calc examples
@@ -82,7 +49,7 @@ for TEST in \
 ; do
     "${TEST}" > /dev/null &
     sleep 1
-    cucumber examples/Calc
+    (cd examples/Calc; cucumber)
     wait %
 done
 
@@ -97,12 +64,10 @@ for TEST in \
     build/examples/CalcQt/QtTestCalculatorQtSteps \
     build/examples/CalcQt/BoostCalculatorQtSteps \
 ; do
-    if [ -n "${DISPLAY:-}" ]; then
-        "${TEST}" 2> /dev/null &
-        sleep 1
-        cucumber examples/CalcQt
-        wait %
-    fi
+    "${TEST}" 2> /dev/null &
+    sleep 1
+    (cd examples/CalcQt; cucumber)
+    wait %
 done
 
 killXvfb
@@ -111,12 +76,11 @@ killXvfb
 # Execute feature showcase on Unix socket
 #
 
-SOCK=cucumber.wire.sock
+SOCK=`pwd`/cucumber.wire.sock
 TEST=build/examples/FeatureShowcase/FeatureShowcaseSteps
 echo "unix: ${SOCK}" > examples/FeatureShowcase/features/step_definitions/cucumber.wire
 "${TEST}" --unix "${SOCK}" > /dev/null &
-cucumber examples/FeatureShowcase
+(cd examples/FeatureShowcase; cucumber)
 wait %
-
 
 cmake --build build --target install
