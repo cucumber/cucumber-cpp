8ec2b388fb88208db160d9974819889374159553
Joerg Kreuzberger <j.kreuzberger@procitec.de>
2024-04-11 09:04:44 +0200

    integrate changes from review

--- src/drivers/QtTestDriver.cpp
+++ src/drivers/QtTestDriver.cpp
@@ -10,30 +10,39 @@ namespace internal {
 const InvokeResult QtTestStep::invokeStepBody() {
     QString file_name;
     {
-        QTemporaryFile file;
+        QTemporaryFile file(QString("%1/%2_%3")
+                                .arg(
+                                    QDir::tempPath(),
+                                    qApp->applicationName().isEmpty() ? "qt_temp"
+                                                                      : qApp->applicationName()
+                                )
+                                .arg(qApp->applicationPid()));
         if (!file.open()) {
             return InvokeResult::failure("Unable to open temporary file needed for this test");
         }
-        file.close();
+        file_name = file.fileName() + ".txt";
     }
 
-    file_name += ".txt";
-
-    QtTestObject testObject( this );
+    QtTestObject testObject(this);
     int returnValue = QTest::qExec(
         &testObject,
         QStringList() << "test"
                       << "-o" << file_name
     );
-    auto ok = ( returnValue == 0 ) ? true : false;
-    std::string error_text;
-    QFile file( file_name );
-    if(!ok) {
-      file.open( QIODevice::ReadOnly );
-      error_text = QString::fromLocal8Bit( file.readAll() ).toStdString();
+
+    const auto ok = returnValue == 0;
+    if (ok) {
+        QFile::remove(file_name);
+        return InvokeResult::success();
+    } else {
+        std::string error_text;
+        QFile file(file_name);
+        file.open(QIODevice::ReadOnly);
+        error_text = QString::fromLocal8Bit(file.readAll()).toStdString();
+        file.close();
+        file.remove();
+        return InvokeResult::failure(error_text);
     }
-    //file.remove();
-    return ok ?  InvokeResult::success() :  InvokeResult::failure( error_text );
 }
 
 }
