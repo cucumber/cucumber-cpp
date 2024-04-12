9d89a24238aac54047183fc5b12a419517e6d36a
Joerg Kreuzberger <j.kreuzberger@procitec.de>
2024-04-12 09:01:25 +0200

    integrate QTemporaryTestDriver suggestion

--- src/drivers/QtTestDriver.cpp
+++ src/drivers/QtTestDriver.cpp
@@ -7,38 +7,64 @@
 namespace cucumber {
 namespace internal {
 
-const InvokeResult QtTestStep::invokeStepBody() {
-    QString file_name;
+
+// wraps the QTemporaryFile creation
+// on Windows the file could not be written as long as QTemporaryFile owner of the file.
+class TemporaryFileWrapper {
+public:
+    static TemporaryFileWrapper create()
     {
-        QTemporaryFile file(QString("%1/%2_%3")
-                                .arg(
-                                    QDir::tempPath(),
-                                    qApp->applicationName().isEmpty() ? "qt_temp"
-                                                                      : qApp->applicationName()
-                                )
-                                .arg(qApp->applicationPid()));
-        if (!file.open()) {
-            return InvokeResult::failure("Unable to open temporary file needed for this test");
+        QTemporaryFile tempFile{};
+
+        if(!tempFile.open()) {
+            return {};
         }
-        file_name = file.fileName() + ".txt";
+
+        return {tempFile.fileName() + ".txt"};
+    }
+
+    TemporaryFileWrapper(): filename{} {
+    }
+
+    TemporaryFileWrapper(QString filename): filename{filename} {
+    }
+
+    ~TemporaryFileWrapper() {
+        QFile::remove(filename);
+    }
+
+    bool exists() const { return !filename.isEmpty(); }
+
+    QString name() const {
+        return filename;
+    }
+
+    QString read() const {
+        QFile file{ filename };
+        if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+            return QString();
+        QTextStream in(&file);
+        return in.readAll();
+    }
+
+private:
+    QString filename;
+};
+
+const InvokeResult QtTestStep::invokeStepBody() {
+    const auto file = TemporaryFileWrapper::create();
+    if (!file.exists()) {
+        return InvokeResult::failure("Unable to open temporary file needed for this test");
     }
 
     QtTestObject testObject{this};
-    const QStringList args{"test", "-o", file_name + ",tap"};
+    const QStringList args{"test", "-o", file.name() + ",tap"};
     const int returnValue = QTest::qExec(&testObject, args);
-    const bool ok = returnValue == 0;
 
-    if (ok) {
-        QFile::remove(file_name);
+    if (returnValue == 0) {
         return InvokeResult::success();
     } else {
-        std::string error_text;
-        QFile file(file_name);
-        file.open(QIODevice::ReadOnly);
-        error_text = QString::fromLocal8Bit(file.readAll()).toStdString();
-        file.close();
-        file.remove();
-        return InvokeResult::failure(error_text);
+        return InvokeResult::failure(file.read().toLocal8Bit());
     }
 }
 
